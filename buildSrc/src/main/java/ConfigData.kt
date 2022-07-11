d80b6a4c7e284081cdcfdb137081c7be9b3840db
souravsaikia <sourav.saikia@tokopedia.com>
2022-07-11 19:29:05 +0530

    added buildSrc

---
+++ buildSrc/src/main/java/ConfigData.kt
@@ -0,0 +1,7 @@
+object ConfigData {
+    const val compileSdkVersion = 32
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+    const val kotlinV = "1.6.10"
+    const val composeVersion = "1.1.0"
+    const val coreKtx = "1.8.0"
+    const val lifeCycleRunTimeKtx = "2.5.0"
+    const val activityCompose = "1.5.0"
+    const val appCompat = "1.4.2"
+    const val materialDesign = "1.6.1"
+    const val junitV = "4.13.2"
+    const val androidXJunitV = "1.1.3"
+    const val espressoVersion = "3.4.0"
+}
