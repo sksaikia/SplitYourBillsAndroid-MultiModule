082b0337a6e7922776fde4a566b1e02addea9d87
souravsaikia <sourav.saikia@tokopedia.com>
2022-09-11 18:29:56 +0530

    Added Empty Contacts State

--- contact_picker/src/main/java/com/example/contact_picker/view/ContactPickerScreen.kt
+++ contact_picker/src/main/java/com/example/contact_picker/view/ContactPickerScreen.kt
@@ -1,6 +1,7 @@
 package com.example.contact_picker.view
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,9 @@ import com.example.design.UnifyText
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -22,8 +26,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.common.R
 import com.example.contact_picker.entity.Contact
 import com.example.contact_picker.util.PermissionUtils
 import com.example.contact_picker.viewModel.ContactsViewModel
@@ -72,6 +78,17 @@ fun ContactPickerScreen(
 
             listOfContacts.clear()
         }, modifier = Modifier.align(CenterHorizontally))
+
+        if (contactsState.contacts?.size == 0) {
+            Column(modifier = Modifier.align(CenterHorizontally)
+            ){
+                Spacer(modifier = Modifier.height(20.dp))
+                Image(painter = painterResource(id = R.drawable.ic_user), contentDescription = "")
+                Spacer(modifier = Modifier.height(20.dp))
+                UnifyText(text = "You don't have any contacts yet ")
+            }
+            
+        }
 
         LazyColumn(
             state = rememberLazyListState(),
