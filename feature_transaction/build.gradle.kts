4db08f53cd881ffde3cea4eef2c811d5b28d0829
souravsaikia <sourav.saikia@tokopedia.com>
2022-10-02 11:31:28 +0530

    Changed dependency

--- feature_transaction/build.gradle.kts
+++ feature_transaction/build.gradle.kts
@@ -80,5 +80,5 @@ dependencies {
 
     implementation(HiltDependencies.hiltNavigation)
 
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
+    implementation(ComposeDependencies.constraintLayoutCompose)
 }
