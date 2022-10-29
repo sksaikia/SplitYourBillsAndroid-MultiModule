bfc79232445b6f752161cce31b4874b31bc2565e
souravsaikia <sourav.saikia@tokopedia.com>
2022-10-29 19:33:44 +0530

    Fixed User Card

--- design/src/main/java/com/example/compositions/UserCard.kt
+++ design/src/main/java/com/example/compositions/UserCard.kt
@@ -1,5 +1,6 @@
 package com.example.compositions
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.design.UnifyText
 
@@ -25,6 +27,7 @@ fun UserCard(
     Row(
         modifier = modifier.padding(horizontal = 20.dp).fillMaxWidth().padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         UnifyText(
             modifier = Modifier
